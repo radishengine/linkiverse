da35a9c6d354d255df993b2aa14bebd78639bfbd
radishengine <radishengine@gmail.com>
2016-12-09 13:52:19 +0000

    Add simple support for headings and suggestions

--- commando.js
+++ commando.js
@@ -15,4 +15,35 @@ define(function() {
   
   loadCss('commando.css');
   
+  var commando = {
+    open: function open(container, heading) {
+      var div = document.createElement('DIV');
+      div.className = 'commando';
+      Object.assign(div, commando.simpleProperties);
+      Object.defineProperties(div, commando.complexProperties);
+      if (heading) {
+        div.addHeading(heading);
+      }
+      container.appendChild(div);
+      return div;
+    },
+    simpleProperties: {
+      addHeading: function(heading) {
+        var el = document.createElement('H3');
+        el.innerText = heading;
+        this.appendChild(el);
+      },
+      suggest: function(suggestion) {
+        var el = document.createElement('DIV');
+        el.className = 'suggestion';
+        el.innerText = suggestion;
+        this.appendChild(el);
+      },
+    },
+    complexProperties: {
+    },
+  };
+  
+  return commando;
+  
 });
