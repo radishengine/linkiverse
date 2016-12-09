cd650b74df47270761e4c3112498a3f1731e5d25
radishengine <radishengine@gmail.com>
2016-12-09 13:53:27 +0000

    Use commando features

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
 require(['commando'], function(commando) {
-
+  
+  'use strict';
+  
   console.log('hello linkiworld');
+
+  commando = commando.open(document.body, 'Linkiverse');
+  
+  commando.suggest('var lc = LinkConstraint.domain("youtube.com");');
 
 });
