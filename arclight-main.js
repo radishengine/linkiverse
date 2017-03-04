38bd69c6c5f7ae8c82ba128a8c57c2a10242f05a
radishengine <radishengine@gmail.com>
2017-03-04 23:27:30 +0000

    Update arclight-main.js

--- arclight-main.js
+++ arclight-main.js
@@ -70,11 +70,11 @@ require(['z/inflate', 'ags/GameView'], function(inflate, GameView) {
         };
         script.type = 'text/javascript';
         script.src = '//archive.org/advancedsearch.php'
-          + '?q=collection:' + self.identifier + '+-subject:"game+creation+software"'
+          + '?q=collection:' + self.identifier + '+ags_format_version:(7 OR 9)'
           + '&fl[]=identifier'
           + '&sort[]=date+asc'
           + '&rows=50'
-          + '&page=2'
+          + '&page=1'
           + '&output=json'
           + '&callback=' + callbackName
           + '&save=yes';
