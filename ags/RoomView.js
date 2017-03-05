3a877c08df065e0fbae86b1738c8a12a22665353
radishengine <radishengine@gmail.com>
2017-03-05 00:33:57 +0000

    Update RoomView.js

--- ags/RoomView.js
+++ ags/RoomView.js
@@ -42,6 +42,62 @@ define(function() {
       Object.defineProperty(this, 'chunks', {value:list});
       return list;
     },
+    get main() {
+      var main;
+      if (this.chunks === null) {
+        main = new RoomMainView(this.buffer, this.byteOffset + 2, this.byteLength - 2);
+      }
+      else {
+        for (var i = 0; i < this.chunks.length; i++) {
+          if (this.chunks[i].type === 'main') {
+            main = new RoomMainView(this.chunks[i].buffer, this.chunks[i].byteOffset, this.chunks[i].byteLength);
+            break;
+          }
+        }
+      }
+      if (main) {
+        main.formatVersion = this.formatVersion;
+      }
+      Object.defineProperty(this, 'main', {value:main});
+      return main;
+    },
+  };
+  
+  function RoomMainView(buffer, byteOffset, byteLength) {
+    this.dv = new DataView(buffer, byteOffset, byteLength);
+    this.bytes = new Uint8Array(buffer, byteOffset, byteLength);
+  }
+  RoomMainView.prototype = {
+    get maxHotspots() {
+      return (this.formatVersion >= 25) ? 50
+        : (this.formatVersion >= 23) ? 30
+        : (this.formatVersion >= 9) ? 20
+        : 16;
+    },
+    get maxObjects() {
+      return 10;
+    },
+    get maxWalkZones() {
+      return this.formatVersion >= 9 ? 16 : 0;
+    },
+    get maxShadowLayers() {
+      return this.formatVersion >= 8 ? 16 : 0;
+    },
+    get bitsPerPixel() {
+      return this.formatVersion >= 12 ? this.dv.getUint16(0, true) * 8 : 8;
+    },
+    get walkbehindBaselines() {
+      var pos = this.formatVersion >= 12 ? 2 : 0;
+      var list = new Array(this.dv.getUint16(pos, true));
+      pos += 2;
+      for (var i = 0; i < list.length; i++) {
+        list[i] = this.dv.getInt16(pos, true);
+        pos += 2;
+      }
+      list.afterPos = pos;
+      Object.defineProperty(this, 'walkbehindBaselines', {value:list});
+      return list;
+    },
   };
   
   return RoomView;
