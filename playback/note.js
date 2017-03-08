3840a4ad077683dc2f23cdc369ccaa13ec7f111d
radishengine <radishengine@gmail.com>
2017-03-08 14:53:20 +0000

    Update note.js

--- playback/note.js
+++ playback/note.js
@@ -48,26 +48,64 @@ define(['require'], function(require) {
     gain: 1,
   };
   
+  function loadNoteData(number) {
+    return fetch(require.toUrl('./note'+number+'.wav'), {cache:'force-cache'})
+    .then(function(req) {
+      return req.arrayBuffer();
+    })
+    .then(function(data) {
+      var samples = new Float32Array((data.byteLength - 44) / 2);
+      if (littleEndianMode) {
+        var samples16 = new Int16Array(data, 44, samples.length);
+        for (var i = 0; i < samples.length; i++) {
+          samples[i] = samples16[i] / 32768;
+        }
+      }
+      else {
+        var dv = new DataView(data);
+        for (var i = 0; i < samples.length; i++) {
+          samples[i] = dv.getInt16(44 + i * 2, true) / 32768;
+        }
+      }
+      return samples;
+    });
+  }
+  
   var note = {
-    load: function(audioContext) {
-      return fetch(require.toUrl('./note1.wav'), {cache:'force-cache'})
-      .then(function(req) {
-        return req.arrayBuffer();
-      })
-      .then(function(data) {
-        var samples = new Float32Array((data.byteLength - 44) / 2);
-        if (littleEndianMode) {
-          var samples16 = new Int16Array(data, 44, samples.length);
-          for (var i = 0; i < samples.length; i++) {
-            samples[i] = samples16[i] / 32768;
+    load: function(audioContext, program, bank) {
+      var noteNumber, parts;
+      switch (program) {
+        case 0x00: default:
+          switch (bank) {
+            case 0x00: default:
+              noteNumber = 1;
+              parts = [
+                [38, 13672, 36, 11989, 1682, -11.3, -4],
+                [43, 13103, 41, 10830, 2272, -9.98, -1],
+                [50, 11168, 48, 9313, 1854, -12.3, 0],
+                [59, 9797, 56, 8309, 1487, -9.8, 0],
+                [67, 10516, 64, 9176, 1139, -6.7, 1],
+                [74, 7588, 73, 6194, 1393, -7.2, 1],
+                [81, 7669, 78, 6447, 1221, -7.7, -1],
+                [88, 7345, 84, 5976, 1368, -7.2, -2],
+                [97, 6442, 92, 5392, 1049, -8.2, 1],
+                [127, 4842, 103, 4686, 155, -9.425, 3],
+              ];
+              break;
           }
-        }
-        else {
-          var dv = new DataView(data);
-          for (var i = 0; i < samples.length; i++) {
-            samples[i] = dv.getInt16(44 + i * 2, true) / 32768;
-          }
-        }
+          break;
+        case 0x03:
+          noteNumber = 2;
+          parts = [
+            [49, 5996, 42, 5284, 711, -9.02, -10],
+            [67, 2924, 62, 2699, 224, -5.04, -10],
+            [78, 2938, 71, 2848, 89, -5.04, -6],
+            [90, 2607, 84, 2585, 21, -5.04, -6],
+            [127, 2004, 96, 1802, 201, -5.04, 7],
+          ];
+          break;
+      }
+      return loadNoteData(1).then(function(samples) {
         var keys = new Array(128);
         var i = -1;
         function doKeys(toKey, len, unity, loopStart, loopLen, attenuation_dB, tune_cents) {
@@ -85,16 +123,9 @@ define(['require'], function(require) {
             key.gain = gain;
           }
         }
-        doKeys(38, 13672, 36, 11989, 1682, -11.3, -4);
-        doKeys(43, 13103, 41, 10830, 2272, -9.98, -1);
-        doKeys(50, 11168, 48, 9313, 1854, -12.3, 0);
-        doKeys(59, 9797, 56, 8309, 1487, -9.8, 0);
-        doKeys(67, 10516, 64, 9176, 1139, -6.7, 1);
-        doKeys(74, 7588, 73, 6194, 1393, -7.2, 1);
-        doKeys(81, 7669, 78, 6447, 1221, -7.7, -1);
-        doKeys(88, 7345, 84, 5976, 1368, -7.2, -2);
-        doKeys(97, 6442, 92, 5392, 1049, -8.2, 1);
-        doKeys(127, 4842, 103, 4686, 155, -9.425, 3);
+        for (var j = 0; j < parts.length; j++) {
+          doKeys.apply(null, parts);
+        }
         return keys;
       });
     },
