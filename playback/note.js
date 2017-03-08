764591f14b35c0807ee059d7ea682ea4c2c141a6
radishengine <radishengine@gmail.com>
2017-03-08 14:04:17 +0000

    Update note.js

--- playback/note.js
+++ playback/note.js
@@ -29,7 +29,7 @@ define(['require'], function(require) {
       node.start(baseTime + startTime);
       node.stop(baseTime + endTime);
       var gain = this.audioContext.createGain();
-      gain.gain.setValueAtTime(1, baseTime + endTime - 1e-2);
+      gain.gain.setValueAtTime(1, baseTime + endTime - 0.1);
       gain.gain.exponentialRampToValueAtTime(1e-4, baseTime + endTime);
       gain.connect(destination);
       node.addEventListener('ended', gain.disconnect.bind(gain));
