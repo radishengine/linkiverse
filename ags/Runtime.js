c383028736f7af7f2d7e662d4960bf56c0b3bb67
radishengine <radishengine@gmail.com>
2017-03-18 15:24:28 +0000

    Create Runtime.js

---
+++ ags/Runtime.js
@@ -0,0 +1,41 @@
+define(function() {
+
+  'use strict';
+  
+  const updateEvent = new CustomEvent('update');
+  
+  function Runtime() {
+    this.element = document.createElement('CANVAS');
+    this.element.width = 320;
+    this.element.height = 200;
+    this.eventTarget.runtime = this;
+    this.eventTarget = this.element;
+    this.update = this.update.bind(this);
+  }
+  Runtime.prototype = {
+    tickMillisecs: 1000/40,
+    get ticksPerSecond() {
+      return Math.round(1000/this.tickMillisecs);
+    },
+    set ticksPerSecond(v) {
+      this.tickMillisecs = 1000/v;
+    },
+    begin: function() {
+      //this.loadRoom(this.game.playerCharacter.room);
+      this.nextTick = performance.now() + this.tickMillisecs;
+      requestAnimationFrame(this.update);
+    },
+    update: function(now) {
+      requestAnimationFrame(this.update);
+      if (now < this.nextTick) return;
+      this.nextTick += this.tickMillisecs;
+      if (this.nextTick < now) {
+        this.nextTick = now + this.tickMillisecs;
+      }
+      this.eventTarget.dispatchEvent(updateEvent);
+    },
+  };
+  
+  return Runtime;
+
+});
