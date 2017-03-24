d46d34e2eef184017662599b624d90a0c5c98679
radishengine <radishengine@gmail.com>
2017-03-24 13:05:14 +0000

    Update util.js

--- ags/util.js
+++ ags/util.js
@@ -2,6 +2,15 @@ define(function() {
 
   'use strict';
   
+  var decodeByteString;
+  if ('TextDecoder' in window) {
+    decodeByteString = new TextDecoder("iso-8859-1");
+    decodeByteString = decodeByteString.decode.bind(decodeByteString);
+  }
+  else {
+    decodeByteString = String.fromCharCode.apply.bind(null);
+  }
+  
   var util = {
     member: function(name, def) {
       var value = def.apply(this);
@@ -12,6 +21,14 @@ define(function() {
         Object.defineProperty(this, name, {value:value, enumerable:true});
       }
       return this;
+    },
+    byteString: function(bytes, offset, length) {
+      switch (arguments.length) {
+        case 0: break;
+        case 1: bytes = bytes.subarray(offset); break;
+        default: bytes = bytes.subarray(offset, offset + length); break;
+      }
+      return decodeByteString(bytes);
     },
   };
   
