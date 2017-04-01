42da326402a396b9f5584d51e1d1a18be35f5f05
radishengine <radishengine@gmail.com>
2017-04-01 02:32:01 +0100

    Update SeeRScript.js

--- ags/SeeRScript.js
+++ ags/SeeRScript.js
@@ -179,7 +179,7 @@ define(['./util'], function(util) {
           symbols = this.def.symbols,
           runtime = this.runtime,
           stack = new DataView(new ArrayBuffer(this.def.stackByteLength)),
-          stackTypes = new Uint8Array(this.def.stackLength/4);
+          stackTypes = new Uint8Array(this.def.stackByteLength/4);
       var stackBytes = new Uint8Array(stack.buffer);
       var registers = new Int32Array(8);
       var registerTypes = new Uint8Array(8);
