893ba626460c7b3a8925ac8c615e653dbe2e2b33
radishengine <radishengine@gmail.com>
2017-04-05 00:05:33 +0100

    Update arclight-main.js

--- arclight-main.js
+++ arclight-main.js
@@ -82,7 +82,7 @@ function(inflate, GameView, RoomView, Runtime, midi, specify) {
         };
         script.type = 'text/javascript';
         script.src = '//archive.org/advancedsearch.php'
-          + '?q=collection:' + self.identifier + '+ags_format_version:(7 OR 9)' // + '+-subject:"game+creation-software"'
+          + '?q=collection:' + self.identifier + '+ags_format_version:11' // + '+-subject:"game+creation-software"'
           + '&fl[]=identifier'
           + '&sort[]=date+asc'
           + '&rows=100'
