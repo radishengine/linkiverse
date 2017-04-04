80f76511d00aeee6054ff3c51bc1530a7fd5ca4d
radishengine <radishengine@gmail.com>
2017-04-04 14:56:47 +0100

    Update SeeRScript.js

--- ags/SeeRScript.js
+++ ags/SeeRScript.js
@@ -584,8 +584,8 @@ define(['./util'], function(util) {
                   copyType = SLOT_INT;
                   break;
                 case SLOT_NAMED_OFFSET:
-                  copyValue = arg2Value;
-                  copyType = SLOT_NAMED_OFFSET;
+                  copyValue = runtime.rawPeek(namedOffsets[arg2Value], 4);
+                  copyType = SLOT_INT;
                   break;
                 default:
                   console.error('NYI: SeeR MOV from type ' + arg2Type);
