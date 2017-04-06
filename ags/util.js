1d5b09ee2ffb843e7b4dd636470e81dcab5bd9db
radishengine <radishengine@gmail.com>
2017-04-06 23:02:09 +0100

    Update util.js

--- ags/util.js
+++ ags/util.js
@@ -11,7 +11,91 @@ define(function() {
     decodeByteString = String.fromCharCode.apply.bind(null);
   }
   
+  function Stack32(maxSize, initialSize) {
+    if (!isNaN(maxSize)) this.maxSize = maxSize;
+    this.dv = new DataView(new ArrayBuffer(isNaN(initialSize) ? 16 : initialSize));
+  }
+  Stack32.prototype = {
+    maxSize: 4000,
+    _pos: 0,
+    _localBase: 0,
+    get absolutePos() {
+      return this._pos;
+    },
+    set absolutePos(pos) {
+      if (!(pos >= 0 && pos < this.stack.length)) {
+        if (isNaN(pos)) {
+          throw new TypeError('absolutePos must be a number, got ' + typeof pos);
+        }
+        throw new RangeError('absolutePos out of range (0-'+(this.stack.length-1)+'): '+pos);
+      }
+      this._pos = pos;
+    },
+    get topRelativePos() {
+      return this._pos - this.dv.byteLength;
+    },
+    set topRelativePos(pos) {
+      if (!(pos <= 0)) {
+        if (isNaN(pos)) {
+          throw new TypeError('pos must be a number');
+        }
+        throw new Error('stack underflow');
+      }
+      var newPos = this.dv.byteLength + pos;
+      if (newPos < 0) {
+        var newSize = this.dv.byteLength;
+        do {
+          if (newSize >= this.maxSize) throw new Error('stack overflow');
+          newPos += newSize;
+          newSize *= 2;
+        } while (newPos < 0);
+        var newDV = new DataView(new ArrayBuffer(newSize));
+        new Uint8Array(newDV.buffer).set(
+          new Uint8Array(this.dv.buffer),
+          newSize - this.dv.byteLength);
+      }
+      this._pos = newPos;
+    },
+    get localPos() {
+      return this.topRelativePos - this._localBase;
+    },
+    set localPos(pos) {
+      this.topRelativePos = this._localBase + pos;
+    },
+    ensureBelow: function(n) {
+      var diff = n - this._pos;
+      if (diff <= 0) return;
+      var newSize = this.dv.byteLength;
+      do {
+        if (newSize >= this.maxSize) throw new Error('stack overflow');
+        diff -= newSize;
+        this._pos += newSize;
+        newSize *= 2;
+      } while (diff > 0);
+      var newDV = new DataView(new ArrayBuffer(newSize));
+      new Uint8Array(newDV.buffer, newSize - this.dv.byteLength).set(new Uint8Array(this.dv.buffer));
+      this.dv = newDV;
+    },
+    pushInt32: function(i) {
+      this.ensureBelow(4);
+      this.dv.setInt32(this._pos -= 4, i, true);
+    },
+    popInt32: function() {
+      var i = this.dv.getInt32(this._pos, true);
+      this._pos += 4;
+      return i;
+    },
+    pushLocalBase: function() {
+      this.pushInt32(this._localBase);
+      this._localBase = this.topRelativePos;
+    },
+    popLocalBase: function() {
+      this._localBase = this.popInt32();
+    },
+  };
+  
   var util = {
+    Stack32: Stack32,
     member: function(name, def) {
       var value = def.apply(this);
       if (typeof value === 'function') {
