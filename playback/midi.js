1356ad3eda537274492fbabf69a262c751412fc4
radishengine <radishengine@gmail.com>
2017-04-14 03:38:52 +0100

    Update midi.js

--- playback/midi.js
+++ playback/midi.js
@@ -251,7 +251,7 @@ define(['./note'], function(noteData) {
     play: function() {
       return midi.load.apply(midi, arguments).then(midi.resume);
     },
-    load: function(source, selectTrackNumber) {
+    load: function(source, selectTrackNumber, keepInitialSilence) {
       if (source instanceof Blob) {
         var fr = new FileReader();
         return new Promise(function(resolve, reject) {
@@ -322,10 +322,17 @@ define(['./note'], function(noteData) {
       if (trackMode === 'separate' && trackCount > 1) {
         foundTracks = [foundTracks[selectTrackNumber]];
       }
+      var minDelta = Infinity;
       for (var i = 0; i < foundTracks.length; i++) {
         foundTracks[i].pos = 0;
         foundTracks[i].nextVarint = nextVarint;
         foundTracks[i].remainingDelta = foundTracks[i].nextVarint();
+        minDelta = Math.min(minDelta, foundTracks[i].remainingDelta);
+      }
+      if (!keepInitialSilence && isFinite(minDelta)) {
+        for (var i = 0; i < foundTracks.length; i++) {
+          foundTracks[i].remainingDelta -= minDelta;
+        }
       }
       activeTracks = foundTracks;
       return Promise.resolve();
