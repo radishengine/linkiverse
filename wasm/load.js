9fe7236bf4f097940e542b0e9c556d3eadf60a56
radishengine <radishengine@gmail.com>
2017-04-29 10:33:39 +0100

    Create load.js

---
+++ wasm/load.js
@@ -0,0 +1,42 @@
+define(['./parse', './encode'], function(wasm_parse, wasm_encode) {
+
+  'use strict';
+  
+  // for plugin use, e.g. require(['wasm/load!./mymodule']) to load ./mymodule.wat
+  
+  // TODO: indexeddb-based caching
+  
+  return {
+    load: function(name, parentRequire, onload, config) {
+      fetch(parentRequire.toUrl(name+'.wat'))
+      .then(function(response) {
+        if (response.ok) return response.text();
+        return Promise.reject(response.url + ': ' + response.status + ' ' + response.statusText);
+      })
+      .then(function(wat) {
+        var wasm_blob = wasm_encode(wasm_parse(wat));
+        return new Promise(function(resolve, reject) {
+          var fr = new FileReader;
+          fr.onerror = function() {
+            reject(fr.error);
+          };
+          fr.onload = function() {
+            resolve(new Uint8Array(fr.result));
+          };
+          fr.readAsArrayBuffer(wasm_blob);
+        });
+      })
+      .then(function(wasm) {
+        if (!WebAssembly.verify(wasm)) {
+          return Promise.reject('invalid wasm');
+        }
+        return WebAssembly.compile(wasm);
+      })
+      .then(onload, function(reason) {
+        if (!(reason instanceof Error)) reason = new Error(reason);
+        onload.error(reason);
+      });
+    },
+  };
+
+});
