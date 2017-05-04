d7a3ce7a8a53256e9cb753d135c5162743247640
radishengine <radishengine@gmail.com>
2017-05-04 13:06:32 +0100

    Update ags_main.js

--- ags_main.js
+++ ags_main.js
@@ -12,8 +12,17 @@ function(require
   var identifiers = Object.keys(iaItems.set);
   for (var i = 0; i < identifiers.length; i++) {
     require(['storage/ia!'+identifiers[i]], function(item) {
-      var collections = [].concat(item.collection || []);
-      var subjects = [].concat(item.subject || []);
+      var collections = [].concat(item.collection || []).filter(function(collection) {
+        if (collection === 'adventuregamestudio') return false;
+        if (collection === 'classicpcgames') return false;
+        if (collection === 'software') return false;
+        if (collection === 'softwarelibrary') return false;
+        return true;
+      });
+      var subjects = [].concat(item.subject || []).filter(function(subject) {
+        if (subject === 'Adventure Game Studio games') return false;
+        return true;
+      });
       var title = item.title;
       var mediatype = item.mediatype;
       console.log(title, mediatype, collections, subjects);
