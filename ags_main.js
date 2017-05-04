45d666ac12185d94c318db94af93ec80f9c04fc8
radishengine <radishengine@gmail.com>
2017-05-04 12:30:45 +0100

    Update ags_main.js

--- ags_main.js
+++ ags_main.js
@@ -1,4 +1,13 @@
-require(['storage/ia!(collection:adventuregamestudio AND mediatype:software)'],
-function(iaItems) {
+requirejs.config({
+  waitSeconds: 0,
+  enforceDefine: true,
+});
+
+requirejs(['require'
+  ,'storage/ia!(collection:adventuregamestudio AND mediatype:software)'
+],
+function(require
+  ,iaItems
+) {
   console.log(iaItems);
 });
