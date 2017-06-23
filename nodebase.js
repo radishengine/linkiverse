d788b28f35b4ad72d729534a3bbbbc66e27403d3
radishengine <radishengine@gmail.com>
2017-06-23 14:04:06 +0100

    Update nodebase.js

--- nodebase.js
+++ nodebase.js
@@ -1,26 +1,7 @@
 
-define(function() {
+define(['idb!nodebase'], function(db) {
 
   'use strict';
-  
-  var gotDB = new Promise(function(resolve, reject) {
-    var opening = indexedDB.open('nodebase', 1);
-    opening.onupgradeneeded = function(e) {
-      var db = e.target.result;
-      var nodeStore = db.createObjectStore('nodes', {autoIncrement:true});
-      nodeStore.createIndex('id', 'id', {unique:false});
-      nodeStore.createIndex('root', 'root', {unique:false});
-      nodeStore.createIndex('root+id', ['root', 'id'], {unique:true});
-      nodeStore.createIndex('root+nodeName', ['root', 'nodeName'], {unique:false});
-      nodeStore.createIndex('root+classList', ['root', 'classList'], {multiEntry:true});
-    };
-    opening.onerror = function(e) {
-      reject();
-    };
-    opening.onsuccess = function(e) {
-      resolve(e.target.result);
-    };
-  });
   
   var nodebase = {};
   
@@ -53,72 +34,64 @@ define(function() {
   
   nodebase.Doc = Doc;
   
-  nodebase.getDB = function() {
-    return gotDB;
-  };
+  nodebase.db = db;
   
   nodebase.getAllRoots = function() {
-    return gotDB.then(function(db) {
-      return new Promise(function(resolve, reject) {
-        var transaction = db.transaction('readonly');
-        var result = [];
-        transaction.oncomplete = function() {
-          resolve(result);
-        };
-        transaction.onabort = function() {
-          reject();
-        };
-        var nodeStore = transaction.objectStore('nodes');
-        nodeStore.index('root').openKeyCursor().onsuccess = function(e) {
-          var cursor = e.target.result;
-          if (!cursor) return;
-          if (cursor.key === cursor.primaryKey) {
-            result.push({key:cursor.key, record:cursor.value});
-          }
-          else {
-            nodeStore.openCursor(IDBKeyRange.only(cursor.key))
-            .onsuccess = function(e) {
-              var cursor = e.target.result;
-              if (cursor) result.push({key:cursor.key, record:cursor.value});
-            };
-          }
-          cursor.continue();
-        };
-      });
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction('readonly');
+      var result = [];
+      transaction.oncomplete = function() {
+        resolve(result);
+      };
+      transaction.onabort = function() {
+        reject();
+      };
+      var nodeStore = transaction.objectStore('nodes');
+      nodeStore.index('root').openKeyCursor().onsuccess = function(e) {
+        var cursor = e.target.result;
+        if (!cursor) return;
+        if (cursor.key === cursor.primaryKey) {
+          result.push({key:cursor.key, record:cursor.value});
+        }
+        else {
+          nodeStore.openCursor(IDBKeyRange.only(cursor.key))
+          .onsuccess = function(e) {
+            var cursor = e.target.result;
+            if (cursor) result.push({key:cursor.key, record:cursor.value});
+          };
+        }
+        cursor.continue();
+      };
     });
   };
   
   nodebase.getRootById = function(id) {
-    return this.getDB().then(function(db) {
-      return new Promise(function(resolve, reject) {
-        var transaction = db.transaction('readonly');
-        var nodesById = transaction.objectStore('nodes').index('id');
-        nodesById.openCursor(IDBKeyRange.only(id)).onsuccess = function(e) {
-          var cursor = e.target.result;
-          if (!cursor) {
-            resolve(null);
-          }
-          else if (cursor.primaryKey !== cursor.value.root) {
-            cursor.continue();
-          }
-          else {
-            resolve({key:cursor.key, record:cursor.value});
-          }
-        };
-      });
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction('readonly');
+      var nodesById = transaction.objectStore('nodes').index('id');
+      nodesById.openCursor(IDBKeyRange.only(id)).onsuccess = function(e) {
+        var cursor = e.target.result;
+        if (!cursor) {
+          resolve(null);
+        }
+        else if (cursor.primaryKey !== cursor.value.root) {
+          cursor.continue();
+        }
+        else {
+          resolve({key:cursor.key, record:cursor.value});
+        }
+      };
     });
   };
   
   nodebase.getRootByKey = function(key) {
-    return this.getDB().then(function(db) {
-      return new Promise(function(resolve, reject) {
-        var transaction = db.transaction('readonly');
-        var nodes = transaction.objectStore('nodes');
-        nodes.openCursor(IDBKeyRange.only(key)).onsuccess = function(e) {
-          var cursor = e.target.result;
-          resolve(cursor ? {key:cursor.key, record:cursor.value} : null);
-        };
-      });
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction('readonly');
+      var nodes = transaction.objectStore('nodes');
+      nodes.openCursor(IDBKeyRange.only(key)).onsuccess = function(e) {
+        var cursor = e.target.result;
+        resolve(cursor ? {key:cursor.key, record:cursor.value} : null);
+      };
     });
   };
   
