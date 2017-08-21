9a323c06c3ef2d6ba17779d4ef7a8adb8a541d6e
radishengine <radishengine@gmail.com>
2017-08-21 23:15:37 +0100

    Update hcworker.js

--- hcworker.js
+++ hcworker.js
@@ -334,6 +334,36 @@ function macDate(dv, offset) {
   return new Date(new Date(1904, 0).getTime() + offset * 1000);
 }
 
+var fixedPoint = {
+  fromInt32: function(i32) {
+    var frac = 0;
+    for (var i = 0; i < 16; i++) {
+      if (i32 & (0x8000 >> i)) {
+        frac += 1 / (2 << i);
+      }
+    }
+    return (i32 >>> 16) + frac;
+  },
+  fromInt32_2_30: function(i32) {
+    var frac = 0;
+    for (var i = 0; i < 30; i++) {
+      if (i32 & (0x20000000 >> i)) {
+        frac += 1 / (2 << i);
+      }
+    }
+    return (i32 >>> 30) + frac;
+  },
+  fromUint16: function(u16) {
+    var frac = 0;
+    for (var i = 0; i < 8; i++) {
+      if (u16 & (0x80 >> i)) {
+        frac += 1 / (2 << i);
+      }
+    }
+    return (u16 >>> 8) + frac;
+  },
+};
+
 function RectView(buffer, byteOffset, byteLength) {
   this.dataView = new DataView(buffer, byteOffset || 0, byteLength || RectView.byteLength);
 }
