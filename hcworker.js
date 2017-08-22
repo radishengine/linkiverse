f98eaba6b65ea8364a277fc5617566ef332fa7fb
radishengine <radishengine@gmail.com>
2017-08-22 12:03:32 +0100

    Update hcworker.js

--- hcworker.js
+++ hcworker.js
@@ -1332,8 +1332,28 @@ var resourceHandlers = {
         bytes.subarray(sampleDataOffset, sampleDataOffset + totalBytes),
         samplingRate,
         channels,
-        bytesPerSample,
+        bytesPerSample
       ),
+    });
+  },
+  ASND: function(item, path, bytes) {
+    var deltas = new Int8Array(bytes.buffer, bytes.byteOffset + 4, 16);
+    bytes = bytes.subarray(20);
+    var samples = new Uint8Array(2 * bytes.length);
+    var value = -128;
+    for (var i = 0; i < bytes.length; i++) {
+      value += deltas[bytes[i] & 0xf];
+      value = value << 24 >> 24;
+      samples[i*2] = value & 0xff;
+      value += deltas[(bytes[i] >> 4) & 0xf];
+      value = value << 24 >> 24;
+      samples[i*2 + 1] = value & 0xff;
+    }
+    postMessage({
+      item: item,
+      path: path,
+      headline: 'file',
+      file: makeWav(samples, 11000, 1, 1),
     });
   },
 };
