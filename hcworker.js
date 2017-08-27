81138b3f35aec7db4fd4884e3835bf65a6b93705
radishengine <radishengine@gmail.com>
2017-08-27 03:00:45 +0100

    Update hcworker.js

--- hcworker.js
+++ hcworker.js
@@ -1959,6 +1959,59 @@ var resourceHandlers = {
       pos += len;
     }
   },
+  ASCN: function(item, path, bytes) {
+    var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+    var designLength = dv.getUint16(0, false);
+    var pos = designLength;
+    var obj = {
+      top: dv.getInt16(pos, false),
+      left: dv.getInt16(pos + 2, false),
+      bottom: dv.getInt16(pos + 4, false),
+      right: dv.getInt16(pos + 6, false),
+      worldY: dv.getInt16(pos + 8, false),
+      worldX: dv.getInt16(pos + 10, false),
+      northBlocked: !!bytes[pos + 12],
+      southBlocked: !!bytes[pos + 13],
+      eastBlocked: !!bytes[pos + 14],
+      westBlocked: !!bytes[pos + 15],
+      soundFrequency: dv.getInt16(pos + 16, false),
+      soundType: bytes[pos + 18],
+    };
+    pos += 20;
+    obj.northMessage = macRoman(bytes, pos+1, bytes[pos]);
+    pos += 1 + obj.northMessage.length;
+    obj.southMessage = macRoman(bytes, pos+1, bytes[pos]);
+    pos += 1 + obj.southMessage.length;
+    obj.eastMessage = macRoman(bytes, pos+1, bytes[pos]);
+    pos += 1 + obj.eastMessage.length;
+    obj.westMessage = macRoman(bytes, pos+1, bytes[pos]);
+    pos += 1 + obj.westMessage.length;
+    obj.soundName = macRoman(bytes, pos+1, bytes[pos]);
+    if (obj.northMessage) postMessage({
+      item: item,
+      path: path,
+      headline: 'text',
+      text: obj.northMessage,
+    });
+    if (obj.southMessage) postMessage({
+      item: item,
+      path: path,
+      headline: 'text',
+      text: obj.southMessage,
+    });
+    if (obj.eastMessage) postMessage({
+      item: item,
+      path: path,
+      headline: 'text',
+      text: obj.eastMessage,
+    });
+    if (obj.westMessage) postMessage({
+      item: item,
+      path: path,
+      headline: 'text',
+      text: obj.westMessage,
+    });
+  },
   ATXT: function(item, path, bytes) {
     var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
     var top = dv.getInt16(0, false);
